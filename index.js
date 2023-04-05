0a4b8ca95af92691e327f4c1a9bf732d0c651c34
lozhka13 <forsho@bk.ru>
2023-04-06 00:29:53 +0400

    added swiper section

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });

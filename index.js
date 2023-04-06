57404ede165680f191a86d5cbccfa12ccfa12c2a
lozhka13 <forsho@bk.ru>
2023-04-06 23:50:13 +0400

    small changes

--- index.js
+++ index.js
@@ -87,6 +87,7 @@ const swiper = new Swiper('.swiper', {
 
   // If we need pagination
   pagination: {
+    clickable: true,
     el: '.swiper-pagination',
   },
 
@@ -95,7 +96,6 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',

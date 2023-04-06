9c600460b291044d17691c57381b803140d54f79
lozhka13 <forsho@bk.ru>
2023-04-06 14:52:22 +0400

    slider fixed

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-'use strict';
+// 'use strict';
 
-const swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
   slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: true,
+
+  // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+    el: '.swiper-pagination',
   },
+
+  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  }
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
+
+
+
+
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 10,
+//   freeMode: false,
+//   // pagination: {
+//   //   el: ".swiper-pagination",
+//   //   clickable: true,
+//   // },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   }
+// });
 
 // Сделать переход к обратной связи

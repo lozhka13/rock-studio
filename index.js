911279dc75f51d70db1688990c399f3baa3ee25e
lozhka13 <forsho@bk.ru>
2023-04-06 12:30:56 +0400

    added teachers section

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  spaceBetween: 10,
+  freeMode: false,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
 });

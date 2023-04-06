f40b1247bae9075153fa76848a52288490ba95d7
lozhka13 <forsho@bk.ru>
2023-04-06 16:33:53 +0400

    added filter

--- index.js
+++ index.js
@@ -1,4 +1,84 @@
-// 'use strict';
+'use strict';
+
+
+const allCards = document.querySelectorAll('.card');
+
+const allFilters = document.querySelector('.section-2__list');
+const allButtonFilter = allFilters.children[0];
+const vocalButtonFilter = allFilters.children[1];
+const guitarButtonFilter = allFilters.children[2];
+const drumsButtonFilter = allFilters.children[3];
+const keysButtonFilter = allFilters.children[4];
+
+function selectedOff() {
+  allButtonFilter.classList.remove('selected');
+  vocalButtonFilter.classList.remove('selected');
+  guitarButtonFilter.classList.remove('selected');
+  drumsButtonFilter.classList.remove('selected');
+  keysButtonFilter.classList.remove('selected');
+}
+
+allButtonFilter.addEventListener('click', (e) => {
+  selectedOff();
+  e.target.classList.add('selected');
+  for (let elem of allCards) {
+    elem.style.display = 'block';
+  }
+})
+
+vocalButtonFilter.addEventListener('click', (e) => {
+  selectedOff();
+  e.target.classList.add('selected');
+  for (let elem of allCards) {
+    if (elem.getAttribute('data-instrument') == 'vocal') {
+      elem.style.display = 'block';
+    }
+    else {
+      elem.style.display = 'none';
+    }
+  }
+})
+
+guitarButtonFilter.addEventListener('click', (e) => {
+  selectedOff();
+  e.target.classList.add('selected');
+  for (let elem of allCards) {
+    if (elem.getAttribute('data-instrument') == 'guitar') {
+      elem.style.display = 'block';
+    }
+    else {
+      elem.style.display = 'none';
+    }
+  }
+})
+
+drumsButtonFilter.addEventListener('click', (e) => {
+  selectedOff();
+  e.target.classList.add('selected');
+  for (let elem of allCards) {
+    if (elem.getAttribute('data-instrument') == 'drums') {
+      elem.style.display = 'block';
+    }
+    else {
+      elem.style.display = 'none';
+    }
+  }
+})
+
+keysButtonFilter.addEventListener('click', (e) => {
+  selectedOff();
+  e.target.classList.add('selected');
+  for (let elem of allCards) {
+    if (elem.getAttribute('data-instrument') == 'keys') {
+      elem.style.display = 'block';
+    }
+    else {
+      elem.style.display = 'none';
+    }
+  }
+})
+
+
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
@@ -21,23 +101,3 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
-
-
-
-
-
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 10,
-//   freeMode: false,
-//   // pagination: {
-//   //   el: ".swiper-pagination",
-//   //   clickable: true,
-//   // },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   }
-// });
-
-// Сделать переход к обратной связи
